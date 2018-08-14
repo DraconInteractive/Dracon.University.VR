c35068b67c4bfa700f5f5b89984b1da6272afcff
RarCeth <rarceth@gmail.com>
2018-08-14 11:13:03 +0800

    asdf

--- Assets/Gauntlet.cs
+++ Assets/Gauntlet.cs
@@ -1,45 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Valve.VR.InteractionSystem;
 
 public class Gauntlet : MonoBehaviour {
-	private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (Hand.AttachmentFlags.ParentToHand) & (~Hand.AttachmentFlags.DetachOthers);
+	private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.DetachOthers);
 	public Vector3 handOffset, rotationOffset;
 
 	public GameObject UIContainer;
+	public GameObject UIOne, UITwo;
 
 	public GameObject newHandPrefab;
 	public GameObject newHand;
 
 	Hand equippedHand = null;
+
+	public float followSpeed, rotateSpeed;
+
+	public UnityEvent GauntletAction;
 	void Update () {
-		if (equippedHand != null && equippedHand.controller != null && equippedHand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)) {
-			UIContainer.SetActive(!UIContainer.activeSelf);
-		} 
 		
+		if (equippedHand != null) {
+			if (equippedHand.controller != null && equippedHand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu)) {
+				UIContainer.SetActive(!UIContainer.activeSelf);
+			}
+
+			if (equippedHand.controller != null && equippedHand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger)) {
+				if (GauntletAction != null) {
+					GauntletAction.Invoke();
+				}
+			}
+		}
 	}
 	private void HandHoverUpdate(Hand hand)
 	{
 		if (hand.GetStandardInteractionButtonDown() || ((hand.controller != null) && hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)))
 		{
-			if (hand.currentAttachedObject != gameObject)
-			{
-				//hand.HoverLock(GetComponent<Interactable>());
-
-				//hand.AttachObject(gameObject, attachmentFlags);
-				OnInteract(hand);
-				//Do not put attachy stuff here, put it in on attached. 
-			}
-			else
-			{
-				hand.DetachObject(gameObject);
-
-				//hand.HoverUnlock(GetComponent<Interactable>());
-
-				//Do not put detachy stuff here, put it in on detached. 
-				
-			}
+			OnInteract(hand);
 		}	
 	}
 
@@ -47,32 +45,34 @@ public class Gauntlet : MonoBehaviour {
 	{
 		GetComponent<Collider>().enabled = false;
 
-		transform.parent = hand.transform;
-		transform.localPosition = handOffset;
-		transform.localRotation = Quaternion.Euler(rotationOffset);
 
 		GetComponent<Interactable>().enabled = false;
 
 		UIContainer.SetActive(true);
 		equippedHand = hand;
-
-		hand.DetachObject(this.gameObject);
-		StartCoroutine(AttachNewController(hand));
-	}
-
-	IEnumerator AttachNewController (Hand hand) {
-		yield return new WaitForSeconds(1);
+		
+		//Spawn new controller
 		GameObject g = Instantiate(newHandPrefab);
-		//hand.AttachObject(g, attachmentFlags);
-		g.transform.parent = hand.transform;
-		g.transform.localPosition = Vector3.zero;
-		g.transform.localRotation = Quaternion.Euler(Vector3.zero);
+		hand.AttachObject(g, attachmentFlags);
+
 		newHand = g;
-		yield break;
+
+		this.transform.SetParent(g.transform);
+		transform.localPosition = handOffset;
+		transform.localRotation = Quaternion.Euler(rotationOffset);
+
 	}
 
-	private void OnDetachedFromHand( Hand hand )
-	{
-
+	public void CycleActiveUI () {
+		if (UIContainer.activeSelf) {
+			if (UIOne.activeSelf) {
+				UIOne.SetActive(false);
+				UITwo.SetActive(true);
+			} else {
+				UIOne.SetActive(true);
+				UITwo.SetActive(false);
+			}
+		}
 	}
+
 }
